2a9c95d2019b8f086e90f4b1454e84f4c0bd8285
Nuttapat Kirawittaya <nuttapat.kirawittaya@gmail.com>
2018-01-10 18:30:04 +0700

    Rendering tile map

--- glcode.js
+++ glcode.js
@@ -126,27 +126,60 @@ function makeMesh(d) {
     // get image to drape
     if (debug) {
         vertices = 16;
-        console.log("p1")
-        var material = new THREE.MeshBasicMaterial({ color: new THREE.Color( 'skyblue' )});
 
-        console.log("p2")
+        var texture = new THREE.TextureLoader()
+        .load(
+            // url
+            assembleUrl(true, [z,x,y]),
+            // onLoad function
+            function(resp){
+                canvas3d.tilesToGet--;
+                finished++;
+
+                scene.remove(placeholder);
+                plane.visible=true;
+
+                if (canvas3d.tilesToGet===0) {
+                    progress.opacity = 0;
+                    console.log('STABLE');
+                    updateTileVisibility();
+                }
+
+            },
+            // onProgress function
+            function() {},
+            // onError function
+            function(err) {
+                console.error(err);
+            }
+        );
+
+        var material = new THREE.MeshBasicMaterial({ map: texture });
+
         var geometry = new THREE.PlaneGeometry(tileSize, tileSize);
 
-        console.log("p3")
+        geometry.vertices = [
+            new THREE.Vector3(-tileSize/2, 0, -tileSize/2),
+            new THREE.Vector3(-tileSize/2, 0, tileSize/2),
+            new THREE.Vector3(tileSize/2, 0, -tileSize/2),
+            new THREE.Vector3(tileSize/2, 0, tileSize/2)
+        ];
+        geometry.faces = [
+            new THREE.Face3(0, 1, 2),
+            new THREE.Face3(1, 3, 2)
+        ];
+        geometry.computeFaceNormals();
+
         var xOffset = (x+0.5)*tileSize - basePlaneDimension/2;
         var yOffset = (y+0.5)*tileSize - basePlaneDimension/2;
 
-        console.log("p4")
-        geometry.translate(xOffset, yOffset, 0);
+        geometry.translate(xOffset, 0, yOffset);
 
-        console.log("p5")
         var plane = new THREE.Mesh(geometry, material);
 
         plane.coords = slashify([z,x,y])
         plane.zoom = z;
-        console.log("p6")
         scene.add(plane)
-        console.log("p7")
     } else {
         var texture = new THREE.TextureLoader()
         .load(
@@ -154,7 +187,6 @@ function makeMesh(d) {
             assembleUrl(true, [z,x,y]),
             // onLoad function
             function(resp){
-                console.log('pppp');
                 canvas3d.tilesToGet--;
                 finished++;
 
@@ -234,7 +266,6 @@ function initializeGL(canvas, eventSource) {
                                                    shading: THREE.SmoothShading });
     var cubeGeometry = new THREE.BoxGeometry(1000, 1000, 1000);
     cube = new THREE.Mesh(cubeGeometry, material);
-    cube.rotation.set(0.785, 0.785, 0.0);
     scene.add(cube);
 
     // ---
